720845d720397dd8f98a93fc9755a81655830555
hanchao <hc198974@163.com>
2021-06-13 20:19:41 +0800

    二手房在售功能已经可以用了，需要慢慢爬取数据，能有1000多项

--- spider_lianjia/spiders/zaishou.py
+++ spider_lianjia/spiders/zaishou.py
@@ -1,71 +1,72 @@
-import requests
-import scrapy
-from lxml import etree
-from scrapy import Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 import json
-from pandas import Series,DataFrame
+from pandas import Series, DataFrame
 from bs4 import BeautifulSoup
 from spider_lianjia.items import SpiderZaishouItem
-import re
 
-def get_xiaoqu(n1,n2):
+
+def get_xiaoqu(n1, n2):
     list = []
     for line in open('data/lianjia.json', 'r', encoding='utf-8'):
         list.append(json.loads(line))
 
     df = DataFrame(list)
-    l=[x for x in set(df['title'])]
-    item=l[n1:n2]
-    print(item)
+    l = [x for x in df['title']]
+    s = [x for x in set(l) if x is not None]
+    s.sort()
+    item = s[n1:n2]
     return item
 
-def get_totalpage(str):
-    html=requests.get('https://dl.lianjia.com/ershoufang/pg1rs'+str)
-    response=etree.HTML(html.text)
-    total_page = response.xpath(
-        "//div[@class='page-box house-lst-page-box']//@page-data")
-    total_page = int(re.search('\d+',total_page[0]).group())
-    return total_page
+
+def get_content(g):
+    guaPai = ''
+    previous = ''
+    for i in g:
+        if i.string == '挂牌时间':
+            guaPai = i.find_next_sibling().string
+        else:
+            if i.string == '上次交易':
+                previous = i.find_next_sibling().string
+
+    return guaPai, previous
+
 
 class ZaishouSpider(CrawlSpider):
     name = 'zaishou'
     allowed_domains = ['dl.lianjia.com']
-    s=get_xiaoqu(0,10)
-    for i in s:
-        page=get_totalpage(i)
-        for j in range(1,page+1):
-            start_urls = ['https://dl.lianjia.com/ershoufang/pg'+str(j)+'rs'+str(i)]
-
+    s = get_xiaoqu(25, 35)
+    current_page = 1
+    start_urls = ['https://dl.lianjia.com/ershoufang/pg1rs%s' % p for p in s]
+    # rules的工作关系：1、各个rule是并列关系，都是从start_urls里的开始界面，先后有一定影响，但scrapy有去重功能，提取两次也只执行一次
+    # 2、有callback的会进入提取的页面执行callback方法，没有callback的会进入该页面重新查找rules规则。
+    # 3、linkExtractor使用的是正则表达式，表达式一定要正确，否则会对判断方向产生误导。
     rules = (
-        Rule(LinkExtractor(allow='./ershoufang/\d+\.html'),callback='parse_item'),  # allow里面是正则表达式
-        # Rule(LinkExtractor(allow='/ershoufang/pg(\d+)(rs[^"]+)'), callback='parse_item')  # allow里面是正则表达式
-
+        Rule(LinkExtractor(allow='/ershoufang/(pg\d+)?rs[^"]+')),
+        Rule(LinkExtractor(allow='/ershoufang/\d+\.html'), callback='parse_item'),
     )
 
     def parse_item(self, response):
-        soup=BeautifulSoup(response.text,'lxml')
-        title = response.xpath('//div[@class="communityName"]/a[1]/text()').get()
+        soup = BeautifulSoup(response.text, 'lxml')
+        title = response.xpath(
+            '//div[@class="communityName"]/a[1]/text()').get()
         totalPrice = response.xpath('//span[@class="total"]/text()').get()
-        unitPrice = response.xpath('//span[@class="unitPriceValue"]/text()').get()
-        room = response.xpath("//div[@class='room']/div[@class='mainInfo']/text()").get()
-        type = response.xpath("//div[@class='mainInfo' and @title!='']/text()").get()
-        area = response.xpath("//div[@class='area' ]/div[@class='mainInfo']/text()").get()
+        unitPrice = response.xpath(
+            '//span[@class="unitPriceValue"]/text()').get()
+        room = response.xpath(
+            "//div[@class='room']/div[@class='mainInfo']/text()").get()
+        type = response.xpath(
+            "//div[@class='mainInfo' and @title!='']/text()").get()
+        area = response.xpath(
+            "//div[@class='area' ]/div[@class='mainInfo']/text()").get()
         quyu = response.xpath("//span[@class='info']/a[1]/text()").get()
         district = response.xpath("//span[@class='info']/a[2]/text()").get()
-        seller=response.xpath("//div[@class='brokerName']/a/text()").get()
-        g=soup.select('.content ul li span')
-        for i in g:
-            if i.string=='挂牌时间':
-                target1=i
-            if i.string=='上次交易':
-                target2=i
+        seller = response.xpath("//div[@class='brokerName']/a/text()").get()
+        g = soup.select('.content ul li span')
 
-        guaPai=target1.find_next_sibling().string
-        previous=target2.find_next_sibling().string
-        item = SpiderZaishouItem(title=title, room=room, area=area, quyu=quyu,district=district,
-                                 totalPrice=totalPrice, unitPrice=unitPrice,previous=previous,type=type,
-                                 guaPai=guaPai, seller=seller)
+        guaPai = get_content(g)[0]
+        previous = get_content(g)[1]
+        item = SpiderZaishouItem(title=title, room=room, area=area, quyu=quyu, district=district,
+                                 totalPrice=totalPrice, unitPrice=unitPrice, type=type,
+                                 seller=seller, guaPai=guaPai, previous=previous)
         yield item
-
