60655202fe7b494e3cb5ee23f8b78b3ecc39c7e8
hc198974 <hc198974@163.com>
2021-08-25 13:25:27 +0800

    修改了spyder-chengjiao里多个bug

--- spider_lianjia/spiders/chengjiao.py
+++ spider_lianjia/spiders/chengjiao.py
@@ -8,35 +8,37 @@ from bs4 import BeautifulSoup
 import re
 import requests
 from lxml import etree
-from urllib.parse import quote,urlencode
+from urllib.parse import quote, urlencode
 import pymongo
 from pandas import DataFrame
 
+
 def get_xiaoqu():
-    #将pymongo数据转换成Dataframe
+    # 将pymongo数据转换成Dataframe
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["db_lianjia"]
     mycol = mydb["chengjiao"]
 
     x = list(mycol.find())
-    df=DataFrame(x)
-    #把没用的id删除掉
-    df=df.drop('_id',axis=1)
+    df = DataFrame(x)
+    # 把没用的id删除掉
+    df = df.drop('_id', axis=1)
     l = [x for x in df['title']]
     s = [x for x in set(l) if x is not None]
     s.sort()
     return s
 
+
 class LianjiaSpider(CrawlSpider):
-    #获得小区列表
-    mongodata=get_xiaoqu()
+    # 获得小区列表
+    mongodata = get_xiaoqu()
     name = 'chengjiao'
     allowed_domains = ['dl.lianjia.com']
     current_page = 1
-    
+
     start_urls = ['https://dl.lianjia.com/chengjiao/pg%s' %
 
-                  p for p in range(1,3)]
+                  p for p in range(1, 6)]
 
     rules = (
         Rule(LinkExtractor(allow='./chengjiao/.+\.html')),  # allow里面是正则表达式
@@ -58,19 +60,22 @@ class LianjiaSpider(CrawlSpider):
                 yield Request(url, callback=self.parse_lianjia)
 
     def parse_lianjia(self, response):
-        quyu = re.sub('二手房成交', '', response.xpath("//div[@class='crumbs fl']/a[3]/text()").get())#获得区域
-        district = re.sub('二手房成交', '', response.xpath("//div[@class='crumbs fl']/a[4]/text()").get())#获得位置
+        quyu = re.sub('二手房成交', '', response.xpath(
+            "//div[@class='crumbs fl']/a[3]/text()").get())  # 获得区域
+        district = re.sub('二手房成交', '', response.xpath(
+            "//div[@class='crumbs fl']/a[4]/text()").get())  # 获得位置
         temp = response.xpath('//div[@class="title"]/a/text()').getall()
         dealDate = response.xpath('//div[@class="dealDate"]/text()').getall()
         totalPrice = response.xpath(
             '//div[@class="totalPrice"]/span/text()').getall()
-        unitPrice = response.xpath('//div[@class="unitPrice"]/span/text()').getall()
+        unitPrice = response.xpath(
+            '//div[@class="unitPrice"]/span/text()').getall()
         dealCycle = response.xpath(
             "//span[@class='dealCycleTxt']/span[2]/text()").getall()
         guaPai = response.xpath(
             "//span[@class='dealCycleTxt']/span[1]/text()").getall()  # 挂牌价有的有有的没有
 
-        soup = BeautifulSoup(response.text, 'lxml')        
+        soup = BeautifulSoup(response.text, 'lxml')
         s = soup.select('.info ')
 
         for i in range(0, len(dealDate)):
@@ -92,8 +97,8 @@ class LianjiaSpider(CrawlSpider):
                     area = temp[i].split()[3]
                 if not title in self.mongodata:
                     item = SpiderLianjiaItem(title=title, room=room, area=area, dealDate=dealDate[i],
-                                         totalPrice=totalPrice[i], unitPrice=unitPrice[i], dealCycle=dealCycle[i],
-                                         guaPai=guaPai[i], seller=seller,quyu=quyu,district=district)
+                                             totalPrice=totalPrice[i], unitPrice=unitPrice[i], dealCycle=dealCycle[i],
+                                             guaPai=guaPai[i], seller=seller, quyu=quyu, district=district)
                     yield item
             except IndexError as e:
                 # 可能会出现有车位的情况，将会发生IndexError
@@ -108,12 +113,13 @@ class LianjiaSpider(CrawlSpider):
 
                 if not title in self.mongodata:
                     item = SpiderLianjiaItem(title=title, room=room, area=area, dealDate=dealDate[i],
-                                         totalPrice=totalPrice[i], unitPrice=unitPrice[i], dealCycle=dealCycle[i],
-                                         guaPai=guaPai[i], seller=seller,quyu=quyu,district=district)
+                                             totalPrice=totalPrice[i], unitPrice=unitPrice[i], dealCycle=dealCycle[i],
+                                             guaPai=guaPai[i], seller=seller, quyu=quyu, district=district)
                     yield item
 
+
 class get_chengjiao_one(object):
-    def get_seller(self,s,*page):
+    def get_seller(self, s, *page):
         base_url = 'https://dl.lianjia.com/chengjiao/display?'
         headers = {
             'Host': 'dl.lianjia.com',
@@ -122,14 +128,14 @@ class get_chengjiao_one(object):
             'X-Requested-With': 'XMLHttpRequest',
         }
         params = {
-            'hid':str(s)
+            'hid': str(s)
         }
         url = base_url + urlencode(params)
         try:
             response = requests.get(url, headers=headers)
             if response.status_code == 200:
                 try:
-                    seller =response.json().get('data').get('name')
+                    seller = response.json().get('data').get('name')
                     return seller
                 except AttributeError as e:
                     return '无'
@@ -137,7 +143,8 @@ class get_chengjiao_one(object):
             print('Error', e.args)
 
     def get_totalpage(self, response):
-        r = response.xpath("//div[@class='page-box house-lst-page-box']/@page-data")
+        r = response.xpath(
+            "//div[@class='page-box house-lst-page-box']/@page-data")
         total_page = re.search('\d+', str(r[0])).group()
         return total_page
 
@@ -148,26 +155,38 @@ class get_chengjiao_one(object):
         l = []
         for x in range(1, (int(total_page) + 1)):
             print(x)
-            html = requests.get('https://dl.lianjia.com/chengjiao/pg' + str(x) + 'rs' + s)
+            html = requests.get(
+                'https://dl.lianjia.com/chengjiao/pg' + str(x) + 'rs' + s)
             r = etree.HTML(html.text)
             list = r.xpath("//div[@class='title']/a/@href")
             for i in list:  # seller是ajax内容
                 # num = re.search('\d+', i).group()
                 # seller=self.get_seller(num)#seller影响速度，而且这个ajax应该是广告
                 response = etree.HTML(requests.get(i).text)
-                title = response.xpath("//div[@class='wrapper']/text()")[0].split()[0]
-                room = response.xpath("//div[@class='wrapper']/text()")[0].split()[1]
-                area = response.xpath("//div[@class='wrapper']/text()")[0].split()[2]
-                totalPrice = response.xpath("//span[@class='dealTotalPrice']/i/text()")[0]
+                title = response.xpath(
+                    "//div[@class='wrapper']/text()")[0].split()[0]
+                room = response.xpath(
+                    "//div[@class='wrapper']/text()")[0].split()[1]
+                area = response.xpath(
+                    "//div[@class='wrapper']/text()")[0].split()[2]
+                totalPrice = response.xpath(
+                    "//span[@class='dealTotalPrice']/i/text()")[0]
                 unitPrice = response.xpath("//div[@class='price']/b/text()")[0]
-                type = response.xpath("//div[@class='base']/div[2]/ul/li[7]/text()")[0]
-                guapaiPrice = response.xpath("//div[@class='msg']/span[1]/label/text()")[0]
-                dealCycle = response.xpath("//div[@class='msg']/span[2]/label/text()")[0]
-                dealDate = re.search('\d+\.\d+\.\d+', response.xpath("//div[@class='wrapper']/span/text()")[0]).group()
-                builtDate = response.xpath("//div[@class='base']/div[2]/ul/li[8]/text()")[0]
-                quyu = response.xpath("//div[@class='deal-bread']/a[3]/text()")[0]
+                type = response.xpath(
+                    "//div[@class='base']/div[2]/ul/li[7]/text()")[0]
+                guapaiPrice = response.xpath(
+                    "//div[@class='msg']/span[1]/label/text()")[0]
+                dealCycle = response.xpath(
+                    "//div[@class='msg']/span[2]/label/text()")[0]
+                dealDate = re.search(
+                    '\d+\.\d+\.\d+', response.xpath("//div[@class='wrapper']/span/text()")[0]).group()
+                builtDate = response.xpath(
+                    "//div[@class='base']/div[2]/ul/li[8]/text()")[0]
+                quyu = response.xpath(
+                    "//div[@class='deal-bread']/a[3]/text()")[0]
                 quyu = quyu[0:len(quyu) - 5]
-                district = response.xpath("//div[@class='deal-bread']/a[4]/text()")[0]
+                district = response.xpath(
+                    "//div[@class='deal-bread']/a[4]/text()")[0]
                 district = district[:len(district) - 5]
                 l.append(
                     [title, totalPrice, unitPrice, room, type, area, quyu, district, builtDate, guapaiPrice, dealCycle,
--- spyder-chengjiao.py
+++ spyder-chengjiao.py
@@ -1,31 +1,37 @@
 from pyquery import PyQuery as pq
 import pymongo
 import re
+import uuid
+
 
 def insert_db(item):
-    client = pymongo.MongoClient(host='localhost',port=27017)
-    db=client['db_lianjia']
-    collection=db['chengjiao']
+    client = pymongo.MongoClient(host='localhost', port=27017)
+    db = client['db_lianjia']
+    collection = db['chengjiao']
     collection.insert_many(item)
 
-def  dropduplicate_db():
-    client = pymongo.MongoClient(host='localhost',port=27017)
-    db=client['db_lianjia']
-    collection=db['chengjiao']
 
-    deleteData=collection.aggregate([{'$group':{'_id':{'dealDate':'$dealDate','title':'$title','room':'$room','area':'$area','totalPrice':'$totalPrice','unitPrice':'$unitPrice'}, 'uniqueIds': { '$addToSet': "$_id" },'count':{'$sum':1}}},{'$match':{'count':{'$gt':1}}}])
-    first=True
+def dropduplicate_db():
+    client = pymongo.MongoClient(host='localhost', port=27017)
+    db = client['db_lianjia']
+    collection = db['chengjiao']
+
+    deleteData = collection.aggregate([{'$group': {'_id': {'dealDate': '$dealDate', 'title': '$title', 'room': '$room', 'area': '$area', 'totalPrice': '$totalPrice',
+                                      'unitPrice': '$unitPrice'}, 'uniqueIds': {'$addToSet': "$_id"}, 'count': {'$sum': 1}}}, {'$match': {'count': {'$gt': 1}}}])
+    first = True
     for d in deleteData:
-        first=True
+        first = True
         for did in d['uniqueIds']:
-            if first!=True:    #第一个不删除
-                collection.delete_one({'_id':did})
-            first=False
+            if first != True:  # 第一个不删除
+                collection.delete_one({'_id': did})
+            first = False
+
 
 def spyder_chengjiao():
     print('开始爬取')
-    list=[]
-    for i in range(1, 4):
+    list = []
+    for i in range(5,6):
+        print(i)
         doc = pq('https://dl.lianjia.com/chengjiao/pg'+str(i))
         p = doc('.listContent li')
         for x in p.items():
@@ -36,26 +42,31 @@ def spyder_chengjiao():
             p4 = x('.dealDate').text()
             p5 = x('.dealCycleTxt span:first-child').text()
             p6 = x('.dealCycleTxt span:last-child').text()
-            
+
             # q=doc(q)
             # p8=q('.dealbread a:first-child').text()
-            q=pq(x('a').attr.href)
-            q=q('.deal-bread a').text()
-            p8=re.sub('二手房成交','',q.split(' ')[2])
-            p9=re.sub('二手房成交','',q.split(' ')[3])
+            q = pq(x('a').attr.href)
+            q = q('.deal-bread a').text()
+            p8 = re.sub('二手房成交', '', q.split(' ')[2])
+            p9 = re.sub('二手房成交', '', q.split(' ')[3])
             try:
-                if len(p7)==3:
-                    item={'title':p7[0], 'room':p7[1], 'area':p7[2],'totalPrice': int(p2)*10000, 'unitPrice':p3, 'dealDate':p4, 'guaPai':p5, 'dealCycle':p6,'seller':'0','district':p8,'quyu':p9}
+                if len(p7) == 3:
+                    item = { 'title': p7[0], 'room': p7[1], 'area': p7[2], 'totalPrice': int(
+                        p2), 'unitPrice': p3, 'dealDate': p4, 'guaPai': p5, 'dealCycle': p6, 'seller': '0', 'district': p9, 'quyu': p8}
+                    list.append(item)
                 else:
-                    item={'title':p7[0], 'room':p7[2], 'area':p7[3], 'totalPrice':int(p2)*10000,'unitPrice': p3,'dealDate': p4,'guaPai': p5,'dealCycle': p6,'seller':'0','district':p8,'quyu':p9}
+                    item = {'title': p7[0], 'room': p7[2], 'area': p7[3], 'totalPrice': int(
+                        p2), 'unitPrice': p3, 'dealDate': p4, 'guaPai': p5, 'dealCycle': p6, 'seller': '0', 'district': p9, 'quyu': p8}
+                    list.append(item)
             except IndexError as e:
-                if p7[1]!='车位':
-                    item={'title':p7[0], 'room':p7[1], 'area':'0', 'totalPrice':int(p2)*10000, 'unitPrice':p3,'dealDate': p4,'guaPai': p5,'dealCycle': p6,'seller':'0','district':p8,'quyu':p9}
-            
-            list.append(item)
+                if p7[1] != '车位':
+                    item = {'title': p7[0], 'room': p7[1], 'area': '0', 'totalPrice': int(
+                        p2), 'unitPrice': p3, 'dealDate': p4, 'guaPai': p5, 'dealCycle': p6, 'seller': '0', 'district': p9, 'quyu': p8}
+                    list.append(item)#这么写是防止如果找不到指，那item就会不变，不变的话，在list里就会有两个重复的值，存入数据库就会发生错误
+
     print('爬取完毕')
     insert_db(list)
 
+spyder_chengjiao()
+print('开始查重')
 dropduplicate_db()
-# spyder_chengjiao()
-
